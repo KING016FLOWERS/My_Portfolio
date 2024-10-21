08507ec3256642e05e3d4a936814a5f2785466a6
Poovarasan H <poova8069@gmail.com>
2024-10-21 18:33:31 +0530

    Portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,355 @@
+
+export const Bio = {
+  name: "Poovarasan H",
+  roles: [
+    "Aspiring Software Developer",
+    "Enthusiastic Full Stack Developer",
+  ],
+  description:
+    "I am a motivated and adaptable individual, always ready to embrace new challenges. With a passion for learning, I am committed to delivering quality results in every task. My positive attitude helps me stay focused and dedicated to achieving my goals. I consistently seek opportunities for growth and improvement. Eager to contribute, I aim to make a meaningful impact in all my efforts.",
+  github: "https://github.com/KING016FLOWERS",
+  resume:
+    "https://drive.google.com/file/d/1pwRqVapF2pU02OdtzhinsjGaph14_QQQ/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/poovarasan-h-5564b52b6/",
+  leetcode:"https://leetcode.com/u/poova8069/",
+  phone: "+91 - 9345059127",
+  email: "poova8069@gmail.com",
+  location: "Chennai, TamilNadu",
+
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image:
+          "https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/js.png?alt=media&token=f595d2d5-e1c5-41ac-b912-dd016a384f16",
+      },
+      {
+        name: "BootStrap",
+        image:
+          "https://www.vectorlogo.zone/logos/getbootstrap/getbootstrap-icon.svg",
+      },
+      {
+        name: "React Js",
+        image:
+          "https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg",
+      },
+      {
+        name: "Tailwind CSS",
+        image: "https://img.icons8.com/?size=100&id=4PiNHtUJVbLs&format=png&color=000000",
+      }
+      
+  
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "C",
+        image: "https://img.icons8.com/?size=100&id=40670&format=png&color=000000",
+      },
+      {
+        name: "Java",
+        image:
+          "https://img.icons8.com/?size=100&id=13679&format=png&color=000000",
+      },
+      {
+        name: "Python",
+        image: "https://img.icons8.com/?size=100&id=13441&format=png&color=000000",
+      }
+    ],
+  },
+  {
+    title: "Database",
+    skills: [
+      {
+        name: "MySQL",
+        image:
+          "https://img.icons8.com/?size=100&id=UFXRpPFebwa2&format=png&color=000000",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://img.icons8.com/?size=100&id=74402&format=png&color=000000",
+      }
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      {
+        name: "Visual Studio Code",
+        image:
+          "https://img.icons8.com/?size=100&id=9OGIyU8hrxW5&format=png&color=000000",
+      },
+      {
+        name: "Git",
+        image:
+          "https://img.icons8.com/?size=100&id=20906&format=png&color=000000",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://img.icons8.com/?size=100&id=AZOZNnY73haj&format=png&color=000000",
+      },
+      {
+        name: "NetBeans IDE",
+        image:
+          "https://img.icons8.com/?size=100&id=4djt356tq8UO&format=png&color=000000",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://img.icons8.com/?size=100&id=QEQQKirln6Tf&format=png&color=000000",
+      }
+    ],
+  },
+  
+];
+
+export const experiences = [
+  {
+    id:0,
+    img: "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/prd.jpeg?alt=media&token=6e264483-341d-437a-bafa-dbe221a59dc0" ,
+    role: "Full Stack Developer Internship",
+    company: "Prodigy InfoTech",
+    date: "Oct 2024 - Present",
+    desc: "During my internship at Prodigy Infotech as a Full Stack Developer, I developed five projects using React.js, Next.js, MongoDB, and various other tools. My work included building a secure user authentication system, designing an employee management platform, and creating a responsive e-commerce website with integrated payment solutions. I also developed a real-time chat app to enhance communication. These projects helped me gain in-depth experience in both front-end and back-end development, improve my ability to design scalable applications, and strengthen my problem-solving skills in a fast-paced environment.",
+    skills: [
+      "React Js",
+      "Next.js",
+      "MongoDB",
+      "Firebase",
+      "Cloudinary",
+      "Pusher",
+      "Paypal Sandbox",
+      "Mailtrap",
+    ],
+  },
+  {
+    id:0,
+    img: "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/clri.jpeg?alt=media&token=740423f2-c32e-4b45-bf2f-0b1e3c6d25a4" ,
+    role: "Project Intern",
+    company: "CSIR- Central Leather Research Institute",
+    date: "Jan 2024 - Apr 2024",
+    desc: "Developed the Workflow-based Single Window Knowledge Product Information System (KPI) for efficient tracking and permitting of knowledge products in a centralized platform. Created the Workflow-based Single Window Pricing Template Automation System (PTA) for automated pricing calculations and streamlined pricing processes. Implemented a Carbon Footprint Measurement initiative to quantify the leather industry’s carbon footprint through comprehensive supply chain data analysis.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Bootstrap",
+      "Java",
+      "JSP",
+      "MySQL",
+      "Apache Tomcat",
+      
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/clricert.jpg?alt=media&token=38c4c5a8-0796-4cb7-bdcb-1531ced4eb49",
+  },
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/openwe.jpeg?alt=media&token=9aefcad3-b764-4c6d-baf1-3fad855062bf",
+    role: "Artificial Intelligence Engineering Intern",
+    company: "OpenWeaver",
+    date: "Aug 2023 - Sept 2023",
+    desc: "Gained practical experience through internships at OpenWeaver and ICT Academy, applying theoretical knowledge to real-world challenges. Augmented AI and machine learning proficiency via self-driven projects.",
+    skills: [
+      "Natural Language Processing",
+      "OpenAI - Summarization",
+      "Openeaver Studio"
+      
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/ict.jpg?alt=media&token=9a9b9c95-ae70-46fd-8864-7bd05360a35a",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/naso.png?alt=media&token=fbb630f2-7556-46c9-ac1d-361ccf01877b",
+    role: "Front End Developer Intern",
+    company: "NASO Technologies pvt. ltd.",
+    date: "June 2023 - July 2023",
+    desc: "Designed Spotify front page layout using ReactJS and CSS, emulating Spotify’s visual elements, with focus on main layout, background styling, and organized song presentation,resulting in an aesthetically pleasing and cohesive user interface",
+    skills: [
+      "ReactJS",
+      "CSS",
+      "MongoDB"
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/naso.jpg?alt=media&token=851b4dc8-d901-4d43-aa04-2a5c7573cf1a",
+
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/unom.jpg?alt=media&token=172f99d9-21bb-4336-ab63-de638b568848",
+    school: "University of Madras Guindy Campus, Chennai",
+    date: "Sept 2022 - Apr 2024",
+    grade: "81.69%",
+    degree: "Master of Science - M.Sc., Computer Science",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/gac.jpeg?alt=media&token=d4a00e71-6696-43a4-a190-2a5041debbf2",
+    school: "Government arts College for Men, Nandanam, Chennai",
+    date: "Jun 2019 - Apr 2022",
+    grade: "94.30%",
+    degree: "Bachelor of Science - B.Sc., Computer Science",
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/mcc.jpg?alt=media&token=ede9a162-28d8-4acb-b74f-0f5c62c88bba",
+    school: "Madras Christian College Hr. Sec. School, Chetpet, Chennai",
+    date: "Jun 2018 - Apr 2019",
+    grade: "74%",
+    degree: "HSC, Maths with Computer Science",
+  },
+  {
+    id: 3,
+    img: "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/tms.jpg?alt=media&token=207a3954-fe2a-4cb7-8db8-6998cac1f5f7",
+    school: "Thangam Matriculation School, Chennai",
+    date: "Jun 2016 - Mar 2017",
+    grade: "94%",
+    degree: "SSLC",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Real-Time Chat Application",
+    date: "Oct 2024",
+    description:
+      "Real-Time Chat app offers private and group chat functionality, instant image sharing, and a real-time messaging experience using Pusher. It’s built with Next.js for fast development, MongoDB for efficient data management, and Next Auth for secure authentication, all wrapped in a sleek UI designed with Tailwind CSS.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/chat.png?alt=media&token=0e0be141-433f-4b93-adac-660db67392b8",
+    tags: [
+      "Next.js",
+      "Tailwind CSS",
+      "MongoDB",
+      "Cloudinary",
+      "Pusher",
+      "Next Auth"
+    ],
+    github: "https://github.com/KING016FLOWERS/Chat_app",
+  },
+  {
+    id: 0,
+    title: "E-Commerce Website",
+    date: "Oct 2024",
+    description:
+      "An E-commerce project includes user authentication and an admin dashboard for managing products, order tracking status. It provides a streamlined product management system for adding, updating, and deleting items. The design uses Tailwind CSS for a responsive interface that adapts to all devices. Cloudinary handles image uploads efficiently to ensure fast delivery. Payment integration is implemented through PayPal Sandbox for secure transactions. The front end is built with Vite and React for optimal performance, and MongoDB serves as the scalable database backend.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/ecm.png?alt=media&token=7fba85b4-f2ab-4e7b-b66d-3ee39c55c943",
+    tags: [
+      "Vite + React",
+      "Tailwind CSS",
+      "Express",
+      "Node.js",
+      "MongoDB",
+      "Cloudinary",
+      "Paypal Sandbox"
+    ],
+    github: "https://github.com/KING016FLOWERS/E-Commerce",
+  },
+  {
+    id: 0,
+    title: "Simple Employee Management System",
+    date: "Oct 2024",
+    description:
+      "The Simple Employee Management System is built using the MERN stack (MongoDB, Express, React, Node.js) and offers a comprehensive solution for managing employees and departments. It features an admin dashboard that provides centralized control over employee and department management. Admins can easily create and manage employee profiles and departments, enabling efficient organizational structure. The system supports CRUD operations, allowing for seamless Create, Read, Update, and Delete functionalities. Additionally, employees can log in with credentials provided by the admin, granting them access to personalized dashboards.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/ems.png?alt=media&token=e6c65c95-a27f-4a31-9935-e8427e55eef7",
+    tags: [
+      "React",
+      "Express",
+      "Node.js",
+      "MongoDB"
+    ],
+    github: "https://github.com/KING016FLOWERS/Simple_Employee_Management_System/",
+  },
+  {
+
+    id: 1,
+    title: "User Authentication",
+    date: "Oct 2024",
+    description:
+      "This project implements a complete authentication system using JWT-based authentication, covering signup, login, and logout functionality to secure user sessions. It also includes email verification via Mailtrap to activate user accounts and a password reset feature for recovering forgotten credentials. With protected routes, access is restricted to authenticated users, ensuring a secure user experience. The system offers a dashboard for logged-in users and uses HTML email templates for seamless communication.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/userauth.png?alt=media&token=3bd07f15-80b2-4996-bda9-de186d29c6e2",
+    tags: [
+      "Vite + React",
+      "Tailwind CSS",
+      "MongoDB",
+      "JSON Web Token",
+      "Mailtrap",
+      "Postman API"
+
+    ],
+    github: "https://github.com/KING016FLOWERS/User-Authentication",
+  },
+  {
+    id: 0,
+    title: "Banking Management System",
+    date: "Sept 2024",
+    description:
+      "The Banking Management System is a Java-based application that simulates an automated teller machine (ATM) using AWT Swing for the user interface and MySQL for database management. This project allows users to perform basic banking operations such as account creation, balance inquiry, cash withdrawal, and transaction history.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/atm.png?alt=media&token=3e0a43d6-243a-4ff0-a5d4-6e39764efdcd",
+    tags: [
+      "Java",
+      "Java-AWT",
+      "Javax-Swing",
+      "MySQL"
+    ],
+    github: "https://github.com/KING016FLOWERS/Banking-Management-System",
+  },
+  
+  {
+    id: 2,
+    title: "Spotify Clone",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Designed Spotify front page layout using ReactJS and CSS, emulating Spotify’s visual elements, with focus on main layout, background styling, and organized song presentation, resulting in an aesthetically pleasing and cohesive user interface.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/spotify.png?alt=media&token=ab1d99b5-0087-4cd1-8eec-03cc5f07d271",
+    tags: [
+      "React Js",
+      "CSS"
+    ],
+    github: "https://github.com/KING016FLOWERS/Spotify-Clone",
+  },
+  {
+    id: 3,
+    title: "Airline Reservation System",
+    date: "Feb 2023 - May 2023",
+    description:
+      "The Airline Reservation System offers secure user registration and authentication, allowing users to create accounts and log in safely. Users can search for available flights by specifying the origin, destination, and travel date. Once they find a suitable flight, they can easily book their tickets and choose their seats. The system also provides booking management, enabling users to view and modify their reservations. Additionally, an admin panel is available for managing flight schedules and bookings, all optimized for responsiveness on both desktop and mobile devices, with deployment on AWS EC2 for high availability.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-app-a38e8.appspot.com/o/ofbms.png?alt=media&token=951242f9-823e-405b-a587-181e82862349",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Bootstrap",
+      "PHP",
+      "MySQL",
+      "AWS-EC2"
+    ],
+    github: "https://github.com/KING016FLOWERS/Airline_reservation_system",
+  },
+];
+
